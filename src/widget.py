733b30c1e4f8513e1ef086a21e08ff0cb5acc9e0
Даниил Чернышов <Dan24012004@yandex.ru>
2025-06-17 14:35:04 +0300

    Реадизация функций

--- src/widget.py
+++ src/widget.py
@@ -1,25 +1,40 @@
-from masks import get_mask_card_number
-
-
 def mask_account_card(card_info: str) -> str:
     """
     Маскирует номер карты, сохраняя тип платежной системы
     """
-    # Разделяю тип и номер карты
+    # Разделяем тип и номер карты
     parts = card_info.split(maxsplit=1)
     card_type = parts[0]
-    card_number = parts[1].replace(" ", "")  # Удаляю пробелы
+    card_number = parts[1].replace(" ", "")  # Удаляем пробелы
 
-    # Применяю маскировку
-    return f"{card_type} {get_mask_card_number(card_number)}"
+    # Маскируем только нужные части номера
+    return f"{card_type} {card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+
+
+def mask_account(account_info: str) -> str:
+    """
+    Маскирует номер счета
+    """
+    # Разделяем тип и номер счета
+    parts = account_info.split(maxsplit=1)
+    account_type = parts[0]
+    account_number = parts[1]
+
+    # Маскируем все, кроме последних 4 цифр
+    return f"{account_type} **{account_number[-4:]}"
 
 
 def get_date(date_str: str) -> str:
     """
-    Преобразует дату из формата ГГГГ-MM-ДД в ДД.MM.ГГГГ
+    Преобразует дату из формата YYYY-MM-DD в DD.MM.YYYY
     """
-    # Разделяю дату на части
-    year, month, day = date_str.split("-")
-    # Формирую новый формат
+    # Разделяем дату и время
+    date_part = date_str.split('T')[0]
+
+    # Разделяем дату на части
+    year, month, day = date_part.split("-")
+
+    # Формируем новый формат
     return f"{day}.{month}.{year}"
+
 
