c02c2a9d912f9afa1bf09abdbe0733a352876d5d
Даниил Чернышов <Dan24012004@yandex.ru>
2025-06-22 14:32:03 +0300

    Сделали финальные изменения

--- src/widget.py
+++ src/widget.py
@@ -1,27 +1,41 @@
-def mask_account_card(card_info: str) -> str:
+
+
+def mask_info(info: str) -> str:
     """
-    Маскирует номер карты
+    Маскирует номер карты или счета в зависимости от типа
     """
-    # Разделяю тип и номер карты
+    # Проверяем, что это счет
+    if "Счет" in info:
+        return mask_account(info)
+
+    # Если это карта
+    return mask_card(info)
+
+
+def mask_card(card_info: str) -> str:
+    """
+    Маскирует номер карты, сохраняя тип платежной системы
+    """
+    # Разделяем тип и номер карты
     parts = card_info.split(maxsplit=1)
     card_type = parts[0]
     card_number = parts[1].replace(" ", "")  # Удаляем пробелы
 
-    # Маскирую только нужные части номера
-    return f"{card_type} {card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    # Маскируем только нужные части номера
+    return f"{card_type} {card_number:4} {card_number4:6}** **** {card_number - 4:}"
 
 
 def mask_account(account_info: str) -> str:
     """
     Маскирует номер счета
     """
-    # Разделяю тип и номер счета
+    # Разделяем тип и номер счета
     parts = account_info.split(maxsplit=1)
-    account_type = parts[0]
-    account_number = parts[1]
+    account_type = parts0
+    account_number = parts1
 
-    # Маскирую все, кроме последних 4 цифр
-    return f"{account_type} **{account_number[-4:]}"
+    # Маскируем все, кроме последних 4 цифр
+    return f"{account_type} **{account_number - 4:}"
 
 
 def get_date(date_str: str) -> str:
