c9022c9fd2563a058dccd96862ccbe240dabdc30
Даниил Чернышов <Dan24012004@yandex.ru>
2025-07-02 00:20:44 +0300

    Исправлены все замечания, домашка готова

--- src/widget.py
+++ src/widget.py
@@ -1,41 +1,19 @@
 
 
-def mask_info(info: str) -> str:
-    """
-    Маскирует номер карты или счета в зависимости от типа
-    """
-    # Проверяем, что это счет
-    if "Счет" in info:
-        return mask_account(info)
+from masks import get_mask_card_number, get_mask_account_number
 
-    # Если это карта
-    return mask_card(info)
-
-
-def mask_card(card_info: str) -> str:
-    """
-    Маскирует номер карты, сохраняя тип платежной системы
-    """
-    # Разделяем тип и номер карты
-    parts = card_info.split(maxsplit=1)
-    card_type = parts[0]
-    card_number = parts[1].replace(" ", "")  # Удаляем пробелы
-
-    # Маскируем только нужные части номера
-    return f"{card_type} {card_number:4} {card_number4:6}** **** {card_number - 4:}"
-
-
-def mask_account(account_info: str) -> str:
-    """
-    Маскирует номер счета
-    """
-    # Разделяем тип и номер счета
-    parts = account_info.split(maxsplit=1)
-    account_type = parts0
-    account_number = parts1
-
-    # Маскируем все, кроме последних 4 цифр
-    return f"{account_type} **{account_number - 4:}"
+def mask_account_card(info: str) -> str:
+    if info.startswith("Счет "):
+        # Работаем со счётом
+        number = info[5:]
+        masked = get_mask_account_number(number)
+        return f"Счет {masked}"
+    else:
+        # Работаем с картой
+        *name_parts, number = info.split()
+        card_name = ' '.join(name_parts)
+        masked = get_mask_card_number(number)
+        return f"{card_name} {masked}"
 
 
 def get_date(date_str: str) -> str:
@@ -50,5 +28,4 @@ def get_date(date_str: str) -> str:
 
     # Формирую новый формат
     return f"{day}.{month}.{year}"
-
 
