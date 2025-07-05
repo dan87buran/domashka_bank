90c57d5858f2da7e4d7bf6109b4a07c719c76c26
Даниил Чернышов <Dan24012004@yandex.ru>
2025-07-05 16:33:58 +0300

    исправил ошибки по файлам и добавил тесты, работвют исправно

--- test_masks.py
+++ test_masks.py
@@ -1,5 +1,5 @@
 
-from masks import get_mask_card_number, get_mask_account  # Исправлено
+from src.masks import  get_mask_card_number, get_mask_account  # Исправлено
 import pytest
 
 
--- test_widget.py
+++ test_widget.py
@@ -1,19 +1,19 @@
 
-from widget import mask_account_card, get_date
+from src.widget import  mask_account, get_date
 import pytest
 from datetime import datetime
 
 
 def test_mask_account_card():
     # Проверяем маскирование карты
-    assert mask_account_card("1234567890123456") == "1234 56** **** 3456"
+    assert mask_account("1234567890123456") == "1234 56** **** 3456"
 
     # Проверяем маскирование счета
-    assert mask_account_card("40817810000000000000") == "4081781000000000****0000"
+    assert mask_account("40817810000000000000") == "4081781000000000****0000"
 
     # Проверяем некорректный ввод
     with pytest.raises(ValueError):
-        mask_account_card("неверный формат")
+        mask_account("неверный формат")
 
 
 
